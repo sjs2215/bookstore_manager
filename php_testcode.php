3930ac039707eb5b65a72d7d5b1779c2b4461b8d
sjs_2215 <soojinsarah@naver.com>
2019-08-27 16:41:18 +0900

    changed servername

--- php_testcode.php
+++ php_testcode.php
@@ -1,6 +1,6 @@
 <?php 
 //mysql server에 접속해
-$conn1=mysqli_connect('localhost', 'root', '154045');
+$conn1=mysqli_connect('127.0.0.1:3307', 'root', '154045');
 
 //fbin을 사용해
 mysqli_select_db($conn1, 'fbin');
@@ -10,14 +10,11 @@ $sql = "SELECT title FROM topic";
 $result = mysqli_query($conn1, $sql);
 
 //전송된 결과를 li 태그로 만들어서 출력해 
-mysqli_fetch_array($result);
-var_dump($row);
 
-mysqli_fetch_array($result);
-var_dump($row);
 
-mysqli_fetch_array($result);
-var_dump($row);
+while($row = mysqli_fetch_array($result)){
+    echo "<li>{$row['title']}</li>";
+}
 
 ?>
 
